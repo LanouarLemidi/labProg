5510c827ebbf80b94d7ef48bebe02ffb3f743a22
Anouar Lahmidi <lahmidianouar@gmail.com>
2024-10-04 12:47:36 -0400

    Fonction ajout

---
+++ public/javascripts/item.js
@@ -0,0 +1,64 @@
+class Item {
+    constructor(nom,prix, itemList){
+        var _date = new Date();
+        this.nom = nom;  
+        this.prix = prix;
+        this.date = String(_date.getMonth() + 1 ).padStart(2,'0') + '/' + String(_date.getDate()).padStart(2,'0') + '/' +String(_date.getFullYear());
+        this.id = itemList.getLastItemId() + 1;
+    }
+}
+
+class ItemList {
+    constructor(){
+        this.tab = [];
+    }
+    add(item){
+        this.tab.push(item);
+    }
+    getLength(){
+        return this.tab.length;
+    }
+    removeItemById(id){
+        for(var i=0; i<this.getLength(); i++){
+            if(this.tab[i].id == id){
+                this.tab.splice(i, 1);
+                return 1;
+            }
+        }
+        return 0;
+    }
+    removeItemByName(nom){
+        for(var i=0; i<this.getLength(); i++){
+            if(this.tab[i].nom == nom){
+                this.tab.splice(i, 1);
+                return 1;
+            }
+        }
+        return 0;
+    }
+    getLastItemId(){
+        var length = this.getLength()
+        if(length > 0)
+            return this.tab[length -1].id;
+        return 0;
+    }
+}
+
+
+
+
+
+
+// console.log("ItemList après ajout de 3 items :", itemList.tab);
+
+// itemList.removeItemById(2);
+// console.log("ItemList après suppression de l'item du milieu (id = 2) :", itemList.tab);
+
+// const item4 = new Item( 'kiwi', '1,50', itemList);
+// itemList.add(item4);
+// console.log("ItemList après ajout d'un nouvel item (kiwi) :", itemList.tab);
+
+// itemList.removeItemByName('banane');
+// console.log("ItemList après suppression du 1er item par nom (banane) :", itemList.tab);
+
+module.exports = {Item, ItemList};
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var {io} = require("../socketApi");
+var {Item, ItemList} = require("../public/javascripts/item");
+
+const itemList = new ItemList();
+itemList.add(new Item('banane', '0,99$', itemList));
+itemList.add(new Item('pomme', '1,20$', itemList));
+itemList.add(new Item('orange', '0,80$', itemList));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Lab 5', list: itemList});
+});
+
+
+
+io.on('connection', function(socket) {
+  socket.on("add", function(data) {
+    itemTmp = new Item(data.nom, data.prix, itemList)
+    console.log(itemTmp);
+    itemList.add(itemTmp);
+    socket.emit('newItem', {id : itemTmp.id, date : itemTmp.date, nom : itemTmp.nom, prix : itemTmp.prix});
+  });
+});
+
+module.exports = router;
